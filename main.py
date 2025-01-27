6d0f758f35f8b0788e10af44645e007261a4b0f0
vladutmocanugabriel <mocanuvladutgabriel@gmail.com>
2025-01-27 22:24:16 +0200

    added the player and asteroid files and methods

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
@@ -8,14 +9,41 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps_management = pygame.time.Clock()
+    dt = 0
+
+    #Creating object groups to manage the player and asteroids
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    #Creating the player
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
-        pygame.Surface.fill(screen, color=(0,0,0))
-
         #Making the window close if the user pressed the [QUIT] button.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        #Filling the screen with black color.    
+        pygame.Surface.fill(screen, color=(0,0,0))
+
+
+
+
+        for obj in updatable:
+            obj.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        # #Update the movement of the player.
+        # player.update(dt)
+        # #Drawing the player and rendering him each frame.
+        # player.draw(screen)
+        
 
 
 
@@ -25,6 +53,14 @@ def main():
 
 
 
+
+
+
+
+
+        #Limit the framerate to 60 FPS
+        dt = fps_management.tick(60) / 1000
+        
         pygame.display.flip()
 
 
